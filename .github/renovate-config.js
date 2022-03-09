bc178c440383c70906409351b74e2636ac108dd9
Malte Jan Kaffenberger <malte@maltejk.de>
2022-03-09 20:40:14 +0100

    feat: hide all screw-ups

---
+++ .github/renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  gitAuthor: 'Renovate Bot <github-renovate@maltejk.de>',
+  onboarding: false,
+  requireConfig: false,
+  platform: 'github',
+  repositories: [
+    'maltejk/helm-charts',
+  ]
+};
